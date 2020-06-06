0a263762aed3e96b78d322595177dc0a632d1b33
mixxwaa <cookies765@gmail.com>
2020-06-07 00:45:24 +0200

    Updates

---
+++ precache-manifest.cfba7cf3133bb51b7203b21b5380c99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49874eb8f442125c6ec35753d33a1e3f",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "8818b1913dabbb95d38a",
+    "url": "/react-movies/static/css/main.475c650b.chunk.css"
+  },
+  {
+    "revision": "c0f570beb2d2a328e9ac",
+    "url": "/react-movies/static/js/2.97a2cf50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies/static/js/2.97a2cf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8818b1913dabbb95d38a",
+    "url": "/react-movies/static/js/main.f4dd91d2.chunk.js"
+  },
+  {
+    "revision": "6e751f6b227e8076d437",
+    "url": "/react-movies/static/js/runtime-main.2e74ae21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-movies/static/media/logo.5d5d9eef.svg"
+  }
+]);
