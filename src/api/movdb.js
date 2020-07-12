f88b56480236192eddf567c3b4373aca14380e0d
mixxwaa <cookies765@gmail.com>
2020-07-12 21:15:19 +0200

    Update movdb.js

--- src/api/movdb.js
+++ src/api/movdb.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //creating axios object with api key , save to env later
-export const api_key = "?api_key=e366d974f73ae203397850eadc7bce1f";
+export const api_key = "API_KEY_HERE";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3/",
 });
