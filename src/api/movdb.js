d85af44d7e8d21514a540b7beda4b19eb9f8de1c
mixxwaa <cookies765@gmail.com>
2020-07-12 21:57:52 +0200

    changed api

--- src/api/movdb.js
+++ src/api/movdb.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //creating axios object with api key , save to env later
-export const api_key = "?api_key=e366d974f73ae203397850eadc7bce1f";
+export const api_key = process.env.REACT_APP_API_KEY;
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3/",
 });
