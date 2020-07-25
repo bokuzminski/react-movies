f272aade24cef5048e414c8aa8d174ef2ecf3034
mixxwaa <cookies765@gmail.com>
2020-07-25 20:42:36 +0200

    Delete movdb.js

--- src/api/movdb.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-//creating axios object with api key , save to env later
-export const api_key = process.env.REACT_APP_API_KEY;
-export default axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-});
