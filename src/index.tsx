eed80460139f56da5ff40e6d50f1c25d2378e9e6
bokuzminski <cookies765@gmail.com>
2022-08-08 21:56:58 +0200

    updated index to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas, faStar } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
@@ -12,12 +12,13 @@ import theme from "./style/theme";
 
 library.add(fas, far, faStar);
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <App />
     </Provider>
     <GlobalStyle />
-  </ThemeProvider>,
-  document.querySelector("#root")
+  </ThemeProvider>
 );
