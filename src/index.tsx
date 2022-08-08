c6c30c632b339986b6af506e5e92a9a4d4127cbe
bokuzminski <cookies765@gmail.com>
2022-08-08 18:37:00 +0200

    changes updated prettier, installed redux toolkit, removed axios, fixed and upgraded some dependencies and removed unnneded ones

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//
-
-import { Store } from './globalState/moviesState';
-import { ThemeProvider } from 'styled-components';
-import theme from './style/theme';
-import GlobalStyle from './style/globals';
-import App from './App';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas, faStar } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { store } from "./redux/store";
+import GlobalStyle from "./style/globals";
+import theme from "./style/theme";
 
 library.add(fas, far, faStar);
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Store>
+    <Provider store={store}>
       <App />
-    </Store>
+    </Provider>
     <GlobalStyle />
   </ThemeProvider>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
