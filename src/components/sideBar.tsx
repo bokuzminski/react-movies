e3896bc3b75568e3aea5fb0f3e3c181bfc646567
bokuzminski <cookies765@gmail.com>
2022-08-08 21:57:17 +0200

    genres fetching

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import StickyBox from 'react-sticky-box';
-import { Link } from 'react-router-dom';
-
-import MenuItem from './MenuItem';
-import LogoImg from './Logo';
-import Loader from './Loader';
-import { useStore } from '../globalState/moviesState';
+import React from "react";
+import { Link } from "react-router-dom";
+import StickyBox from "react-sticky-box";
+import styled from "styled-components";
+import Loader from "./Loader";
+import LogoImg from "./Logo";
+import MenuItem from "./MenuItem";
+import { Genre, useFetchAvailableGenresQuery } from "./redux/genres";
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,7 +18,7 @@ const Wrapper = styled.div`
 `;
 
 const Heading = styled.h2`
-  font-family: 'Proza Libre', sans-serif;
+  font-family: "Proza Libre", sans-serif;
   font-weight: 500;
   font-size: 1.1rem;
   text-transform: uppercase;
@@ -37,26 +36,24 @@ const LinkWrap = styled(Link)`
   margin-bottom: 0.5rem;
 `;
 
-const Sidebar = () => {
-  const [state, dispatch] = useStore();
+export const Sidebar: React.FunctionComponent = () => {
+  const { isLoading, data: genres } = useFetchAvailableGenresQuery();
 
   return (
     <StickyBox>
       <Wrapper>
         <LogoImg />
         <Heading>Genres</Heading>
-        {!state.genres.genres ? <Loader /> : renderGenres(state.genres.genres)}
+        {isLoading ? <Loader /> : renderGenres(genres || [])}
       </Wrapper>
     </StickyBox>
   );
 };
 
-function renderGenres(genres) {
-  return genres.map((genre) => (
+function renderGenres(genres: Genre[]) {
+  return genres.map(genre => (
     <LinkWrap to={`/genres/${genre.name}`} key={genre.id}>
-      <MenuItem key={genre.id} title={genre.name} />
+      <MenuItem title={genre.name} />
     </LinkWrap>
   ));
 }
-
-export default Sidebar;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { genresApi } from "./genres";
 
-export const store = configureStore({ reducer: {} });
-
+export const store = configureStore({
+  reducer: {
+    [genresApi.reducerPath]: genresApi.reducer
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(genresApi.middleware)
+});
+setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
