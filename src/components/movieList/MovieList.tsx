d042b51de2ef79498cb1d5de6a92ec14aba81119
bokuzminski <bogdankuzminski@gmail.com>
2024-08-03 14:10:04 +0200

    movie list using mui grid and pagination

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,40 +1,24 @@
+import { Grid, Pagination } from "@mui/material";
 import React from "react";
 import { Loader } from "src/components/loader/Loader";
 import { MovieListItem } from "src/components/movieList/movieListItem/MovieListItem";
 import { BatchMoviesResponse } from "src/redux/movdbModel";
-import styled from "styled-components";
-
-const MoviesWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(10rem, 25rem));
-  justify-content: space-evenly;
-  align-content: space-between;
-  align-items: start;
-  padding: 4rem 0;
-  grid-gap: 4rem 2rem;
-  @media ${props => props.theme.mediaQueries.small} {
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 23rem));
-    justify-content: space-around;
-    grid-gap: 4rem 1.5rem;
-  }
-  @media ${props => props.theme.mediaQueries.smaller} {
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 18rem));
-    grid-gap: 4rem 1rem;
-  }
-`;
 
 export const MovieList = ({ isLoading, movies }: MovieListProps) => {
   if (isLoading || !movies) return <Loader />;
 
   return (
-    <>
-      <MoviesWrapper>
-        {movies.map(movie => (
+    <Grid container justifyContent="center" alignItems="center" spacing={2}>
+      {movies.map(movie => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
           <MovieListItem key={movie.id} title={movie.title} id={movie.id} poster_path={movie.poster_path} />
-        ))}
-      </MoviesWrapper>
-      {/* <Pagination film={film} /> */}
-    </>
+        </Grid>
+      ))}
+      <Grid item xs={12} container justifyContent="center">
+        {/* TODO: Pagination loginc*/}
+        <Pagination count={15} shape="rounded" size="large" sx={{ mt: 4 }} />
+      </Grid>
+    </Grid>
   );
 };
 
