adb28c8623d06578de9b4f453b10e97fdcecc2fb
bokuzminski <bogdankuzminski@gmail.com>
2024-08-09 01:11:16 +0200

    change how movie list is handled, pagination is on parent

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,8 +1,8 @@
-import { Grid, Pagination, Skeleton } from "@mui/material";
+import { Grid } from "@mui/material";
 import { BatchMoviesResponse } from "src/api/types/movDbTypes";
 import { MovieListItem } from "src/components/movieList/movieListItem/MovieListItem";
 
-export const MovieList = ({ isLoading, movies, setPage, page }: MovieListProps) => {
+export const MovieList = ({ movies }: MovieListProps) => {
   return (
     <Grid container rowGap={3} alignItems="start">
       {movies.map(movie => (
@@ -20,31 +20,10 @@ export const MovieList = ({ isLoading, movies, setPage, page }: MovieListProps) 
           <MovieListItem key={movie.id} title={movie.title} id={movie.id} poster_path={movie.poster_path} />
         </Grid>
       ))}
-      <Grid item xs={12} container justifyContent="center">
-        {/* TODO: Pagination loginc*/}
-        <Pagination
-          count={500}
-          shape="rounded"
-          size="large"
-          sx={{
-            mt: 4,
-            "& .MuiPaginationItem-root": {
-              fontSize: "1.5rem",
-              padding: "10px 20px",
-              margin: "0 5px"
-            }
-          }}
-          defaultPage={page}
-          onChange={(_e, page) => setPage(page)}
-        />
-      </Grid>
     </Grid>
   );
 };
 
 type MovieListProps = {
-  isLoading: boolean;
   movies: BatchMoviesResponse["results"];
-  page: number;
-  setPage: (page: number) => void;
 };
--- src/pages/PopularMovies.tsx
+++ src/pages/PopularMovies.tsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
 import { useFetchPopularMovies } from "../api/hooks";
 import { MovieList } from "../components/movieList/MovieList";
+import { Pagination } from "@mui/material";
 
 export const PopularMovies = () => {
   const [page, setPage] = useState(1);
   const { data, isFetching } = useFetchPopularMovies(page);
-  if (isFetching) return <h1>Loading...</h1>;
 
-  return <MovieList isLoading={isFetching} movies={data?.results} page={page} setPage={setPage} />;
+  if (isFetching) return <h1>Loading...</h1>;
+  if (!data) return <h1>Error: no data to display.</h1>;
+
+  return (
+    <>
+      <MovieList movies={data.results} />
+      <Pagination
+        count={500}
+        shape="rounded"
+        size="large"
+        sx={{
+          mt: 4,
+          "& .MuiPaginationItem-root": {
+            fontSize: "1.5rem",
+            padding: "10px 20px",
+            margin: "0 5px"
+          }
+        }}
+        defaultPage={page}
+        onChange={(_e, page) => setPage(page)}
+      />
+    </>
+  );
 };
