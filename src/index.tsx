dd7ac935b815b64329e4659eef4a72622b2e02e0
Bogdan Kuzminski <bogdan.kuzminski@smartly.io>
2026-03-15 09:47:59 +0100

    feat: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App } from "src/App";
+import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000
   },
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       src: path.resolve(__dirname, "src")
     }
   }
